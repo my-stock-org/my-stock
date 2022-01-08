5870c6a8e50e99d6e84e8e60c47f354a01bb4e2f
RoosveltK <roosvelt.kenne.9817@facsciences-uy1.cm>
2022-01-08 18:57:58 +0100

    It good

--- src/my_stock/Accueil.java
+++ src/my_stock/Accueil.java
@@ -3,12 +3,11 @@ package my_stock;
 public class Accueil {
 
 	public static void main(String[] args) {
-//		BdConnection testConnection = null;
-//		String driverType = "com.mysql.jdbc.Driver";
-//		String url="jdbc:mysql://localhost/mystock";
+		BdConnection testConnection = null;
+		String driverType = "com.mysql.jdbc.Driver";
+		String url="jdbc:mysql://localhost/mystock";
 
-//		testConnection.getInstance(driverType,url, "root", "");
-		System.out.print("go");
+		testConnection.getInstance(driverType,url, "root", "");
 	}
 
 
