c61f7f5388a4e332d22d257b92633dd4580f4181
RoosveltK <roosvelt.kenne.9817@facsciences-uy1.cm>
2022-01-08 13:42:09 +0100

    project structure

---
+++ src/my_stock/Accueil.java
@@ -0,0 +1,10 @@
+package my_stock;
+
+public class Accueil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/my_stock/Commandes.java
@@ -0,0 +1,5 @@
+package my_stock;
+
+public class Commandes {
+
+}
