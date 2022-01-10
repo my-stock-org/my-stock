0adf083645589ac861a432b3cf2cb3941192f202
Oldrich <oldrichngnitedem4@gmail.com>
2022-01-10 17:01:02 +0100

    Interface produit+connection a la base de donnees

---
+++ src/Frame/Accueil.java
@@ -0,0 +1,12 @@
+package Frame;
+
+
+public class Accueil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fenetre produit = Produit.getInstance();
+//		ProduitFrame prd = new ProduitFrame();
+	}
+
+}
---
+++ src/Proprietes/Commandes.java
@@ -0,0 +1,5 @@
+package Proprietes;
+
+public class Commandes {
+
+}
