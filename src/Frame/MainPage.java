eb85d7d004880cba4d8154a70f5e6a77501df1b9
RoosveltK <roosvelt.kenne.9817@facsciences-uy1.cm>
2022-01-27 15:25:14 +0100

    before merge with ngnitedem

--- src/Frame/MainPage.java
+++ src/Frame/MainPage.java
@@ -5,7 +5,7 @@ public class MainPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Fenetre produit = Produit.getInstance();
-		Welcome.getInstance();
+		Accueil.getInstance();
 		// CreateCommande.getInstance();
 		// AfficheCommande.getInstance();
 
